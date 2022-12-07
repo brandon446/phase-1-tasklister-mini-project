3e43045fa28da6781fc2f76d7f9cf0e17f1162d6
brandon <brandon.ngongolo@student.moringaschool.com>
2022-12-07 08:40:54 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,66 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// your code here
+let form =document.getElementById("create-task-form")
+form.addEventListener("submit", (e) =>{
+  e.preventDefault(
+    brandon(document.getElementById("new-task-description").value))
+    form.reset()
+})
+})
+function brandon(myList){
+  let li=document.createElement("li")
+  let btn=document.createElement("button")
+  btn.textContent= " x "
+  btn.addEventListener("click" , cancel)
+  li.textContent= ` ${myList}`
+  li.appendChild(btn)
+  console.log(li)
+  document.querySelector("#list").appendChild(li)
+
+}
+function cancel(e){
+  e.target.parentNode.remove()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
